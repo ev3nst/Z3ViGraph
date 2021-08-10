a20e28d5afee841befe244db977016b691889913
Burak Kartal <burak.kartal@kanal7.com>
2021-08-10 11:29:38 +0300

    Migrations removed. Indentation .vscode tasks configuration. Identity ignore fields.

--- ViGraph.Database/Data/ApplicationDbContext.cs
+++ ViGraph.Database/Data/ApplicationDbContext.cs
@@ -12,46 +12,54 @@ using ViGraph.Models;
 
 namespace ViGraph.Database
 {
-    // Custom type mapping plugin:
-    public class EnumTypeMappingSourcePlugin : IRelationalTypeMappingSourcePlugin
-    {
-        private readonly IMySqlOptions _options;
+	// Custom type mapping plugin:
+	public class EnumTypeMappingSourcePlugin : IRelationalTypeMappingSourcePlugin
+	{
+		private readonly IMySqlOptions _options;
 
-        public EnumTypeMappingSourcePlugin(IMySqlOptions options) => _options = options;
+		public EnumTypeMappingSourcePlugin(IMySqlOptions options) => _options = options;
 
-        /// <summary>
-        /// Return a string type mapping with an `enum` store type, if an `enum()` type has been specified explicitly as the column type.
-        /// </summary>
-        public RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
-            => string.Equals(mappingInfo.StoreTypeNameBase, "enum", StringComparison.OrdinalIgnoreCase)
-                ? new MySqlStringTypeMapping(mappingInfo.StoreTypeName, _options, StoreTypePostfix.None)
-                : null;
-    }
+		/// <summary>
+		/// Return a string type mapping with an `enum` store type, if an `enum()` type has been specified explicitly as the column type.
+		/// </summary>
+		public RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
+			=> string.Equals(mappingInfo.StoreTypeNameBase, "enum", StringComparison.OrdinalIgnoreCase)
+				? new MySqlStringTypeMapping(mappingInfo.StoreTypeName, _options, StoreTypePostfix.None)
+				: null;
+	}
 
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
 
-        }
-        public DbSet<AppUser> AppUser { get; set; }
+		}
+		public DbSet<AppUser> AppUser { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<IdentityUser>().ToTable("Users");
-            modelBuilder.Entity<IdentityUser>()
-                .Ignore(c => c.UserName)
-                .Ignore(c => c.NormalizedUserName)
-                .Ignore(c => c.NormalizedEmail)
-                .Ignore(c => c.EmailConfirmed)
-                .Ignore(c => c.ConcurrencyStamp)
-                .Ignore(c => c.PhoneNumber)
-                .Ignore(c => c.PhoneNumberConfirmed)
-                .Ignore(c => c.LockoutEnabled)
-                .Ignore(c => c.LockoutEnd)
-                .Ignore(c => c.TwoFactorEnabled)
-                .Ignore(c => c.AccessFailedCount);
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<IdentityUser>().ToTable("Users");
+			modelBuilder.Entity<IdentityUser>()
+				.Ignore(c => c.UserName)
+				.Ignore(c => c.NormalizedUserName)
+				.Ignore(c => c.NormalizedEmail)
+				.Ignore(c => c.EmailConfirmed)
+				.Ignore(c => c.ConcurrencyStamp)
+				.Ignore(c => c.PhoneNumber)
+				.Ignore(c => c.PhoneNumberConfirmed)
+				.Ignore(c => c.LockoutEnabled)
+				.Ignore(c => c.LockoutEnd)
+				.Ignore(c => c.TwoFactorEnabled)
+				.Ignore(c => c.AccessFailedCount);
+
+			modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+			modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+			modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+			modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+
+			modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+			modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+		}
+	}
 }
--- ViGraph/Startup.cs
+++ ViGraph/Startup.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.AspNetCore.Identity;
 
 using ViGraph.Database;
@@ -17,7 +17,6 @@ using ViGraph.Models;
 
 namespace ViGraph
 {
-
 	public class Startup
 	{
 		public Startup(IConfiguration configuration)
@@ -31,38 +30,44 @@ namespace ViGraph
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
-            ConfigureEntityFramework(services);
+			services.AddResponseCompression(options => {
+				options.Providers.Add<BrotliCompressionProvider>();
+				options.Providers.Add<GzipCompressionProvider>();
+				options.MimeTypes =
+					ResponseCompressionDefaults.MimeTypes.Concat(
+						new[] { "image/svg+xml", "application/javascript+json+xml", "text/html+css+json+plain+xml" });
+			});
+			ConfigureEntityFramework(services);
 
-            // Not Working ENUM implementation
-            // services.AddSingleton<IRelationalTypeMappingSourcePlugin, EnumTypeMappingSourcePlugin>();
+			// Not Working ENUM implementation
+			// services.AddSingleton<IRelationalTypeMappingSourcePlugin, EnumTypeMappingSourcePlugin>();
 
-            services.AddIdentity<AppUser,IdentityRole>()
-                .AddDefaultTokenProviders()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddHttpContextAccessor();
-            services.AddSession(Options =>
-            {
-                Options.IdleTimeout = TimeSpan.FromMinutes(10);
-                Options.Cookie.HttpOnly = true;
-                Options.Cookie.IsEssential = true;
-            });
+			services.AddIdentity<AppUser, IdentityRole>()
+				.AddDefaultTokenProviders()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddHttpContextAccessor();
+			services.AddSession(Options => {
+				Options.IdleTimeout = TimeSpan.FromMinutes(10);
+				Options.Cookie.HttpOnly = true;
+				Options.Cookie.IsEssential = true;
+			});
 		}
 
-        public void ConfigureEntityFramework(IServiceCollection services)
-        {
-            services.AddDbContextPool<ApplicationDbContext>(
-                options => options.UseMySql(
-                    Configuration.GetConnectionString("DefaultConnection"),
-                    ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection")),
-                    mysqlOptions => {
-                        mysqlOptions.MaxBatchSize(int.Parse(Configuration.GetSection("MaxBatchSize").Value));
-                        int retryOnFail = int.Parse(Configuration.GetSection("RetryOnFail").Value);
-                        if (retryOnFail > 0) {
-                            mysqlOptions.EnableRetryOnFailure(retryOnFail, TimeSpan.FromSeconds(5), null);
-                        }
-                    }
-            ));
-        }
+		public void ConfigureEntityFramework(IServiceCollection services)
+		{
+			services.AddDbContextPool<ApplicationDbContext>(
+				options => options.UseMySql(
+					Configuration.GetConnectionString("DefaultConnection"),
+					ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection")),
+					mysqlOptions => {
+						mysqlOptions.MaxBatchSize(int.Parse(Configuration.GetSection("MaxBatchSize").Value));
+						int retryOnFail = int.Parse(Configuration.GetSection("RetryOnFail").Value);
+						if (retryOnFail > 0) {
+							mysqlOptions.EnableRetryOnFailure(retryOnFail, TimeSpan.FromSeconds(5), null);
+						}
+					}
+			));
+		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -71,17 +76,18 @@ namespace ViGraph
 				app.UseDeveloperExceptionPage();
 			} else {
 				app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
 			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 			app.UseStaticFiles();
+			app.UseResponseCompression();
 
 			app.UseRouting();
-            app.UseAuthentication();
+			app.UseAuthentication();
 			app.UseAuthorization();
-            app.UseSession();
+			app.UseSession();
 
 			app.UseEndpoints(endpoints => {
 				endpoints.MapControllerRoute(
