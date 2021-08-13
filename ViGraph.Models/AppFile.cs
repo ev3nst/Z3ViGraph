3117bb70044734272100b6ab9bb13b606cc9eb35
Burak Kartal <burak.kartal@kanal7.com>
2021-08-13 17:56:44 +0300

    indentation fix try

--- ViGraph.Models/AppFile.cs
+++ ViGraph.Models/AppFile.cs
@@ -4,45 +4,46 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ViGraph.Models
 {
-    public enum AppFileStatus {
-        Initialized,
-        Paused,
-        Completed,
-        Failed
-    }
+	public enum AppFileStatus
+	{
+		Initialized,
+		Paused,
+		Completed,
+		Failed
+	}
 
 	public class AppFile
 	{
-        [Key]
-        public int Id { get; set; }
+		[Key]
+		public int Id { get; set; }
 
 		[Required]
-        [MaxLength(255)]
-        public string Name { get; set; }
+		[MaxLength(255)]
+		public string Name { get; set; }
 
 		[Required]
-        [MaxLength(255)]
-        public string OriginalName { get; set; }
+		[MaxLength(255)]
+		public string OriginalName { get; set; }
 
 		[Required]
-        [MaxLength(255)]
-        public string Folder { get; set; }
+		[MaxLength(255)]
+		public string Folder { get; set; }
 
 		[Required]
-        [MaxLength(50)]
-        public string Mime { get; set; }
+		[MaxLength(50)]
+		public string Mime { get; set; }
 
 		[Required]
-        [Range(1, 137_438_883_103)]
-        public int TotalSize { get; set; }
+		[Range(1, 137_438_883_103)]
+		public int TotalSize { get; set; }
 
 		[Required]
-        [Range(0, 137_438_883_103)]
-        public int UploadedSize { get; set; } = 0;
+		[Range(0, 137_438_883_103)]
+		public int UploadedSize { get; set; } = 0;
 
 		[Required]
 		[Column(TypeName = "enum('Initialized','Paused', 'Completed', 'Failed')")]
-        public AppFileStatus Status { get; set; } = AppFileStatus.Initialized;
+		public AppFileStatus Status { get; set; } = AppFileStatus.Initialized;
 
 		[Required]
 		public DateTime CreatedAt { get; set; } = DateTime.Now;
